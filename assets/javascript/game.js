c0aae49cfb32e867379093bc071804f1f2f29058
Mike Smith <hockeydad66@live.com>
2019-10-21 22:15:20 -0600

    working on html layout

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+//                      Crystal Collector Game
+// Psuedocode
+
+//1. Assign/define variables.
+    //a. Crystal values.
+    var blueGem = 0
+    var purpleGem = 0
+    var whiteGem = 0
+    var greenGem = 0
+        //random values between 1-12.
+        //New value assigned at game start.
+        //Value hidden until assigned crystal button is clicked.
+        document.getElementById ("card-text-green").textContent="Current Value: " + greenGem;
+    //b. Player score.
+    //c. Player wins/losses.
+    //d. Target value to be guessed.
+        // Randomly generated between 19-120
+        // Resets to new value a game start
+
